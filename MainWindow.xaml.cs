669261cc16e86d2d088995e40f4875ecb11a6e61
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-26 22:59:38 +0530

    Get the bhavik university

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,7 +28,8 @@ namespace SQLAndLINQ
             string connectionString = ConfigurationManager.ConnectionStrings["SQLAndLINQ.Properties.Settings.DBDemoConnectionString"].ConnectionString;
             linqToDataClasses1DataContext = new LinqToDataClasses1DataContext(connectionString);
 
-            InsertLectures();
+            //InsertStudentLecture();
+            GetBhavikUniversity();
            
         }
 
@@ -80,13 +81,64 @@ namespace SQLAndLINQ
 
             linqToDataClasses1DataContext.ExecuteCommand("delete from Lecture");
 
-            linqToDataClasses1DataContext.Lectures.InsertOnSubmit(new Lecture { Name="Math"});
+            linqToDataClasses1DataContext.Lectures.InsertOnSubmit(new Lecture { Name= "Math"});
             linqToDataClasses1DataContext.Lectures.InsertOnSubmit(new Lecture { Name = "Phy" });
             linqToDataClasses1DataContext.Lectures.InsertOnSubmit(new Lecture { Name = "Chem" });
             linqToDataClasses1DataContext.Lectures.InsertOnSubmit(new Lecture { Name = "Bio" });
 
             linqToDataClasses1DataContext.SubmitChanges();
             MainDataGrid.ItemsSource = linqToDataClasses1DataContext.Lectures;
+
+        }
+
+        public void InsertStudentLecture()
+        {
+
+            try
+            {
+                STUDENT bhavikVashi = linqToDataClasses1DataContext.STUDENTs.First(student => student.Name.Equals("Bhavik Vashi"));
+                STUDENT rajPatel = linqToDataClasses1DataContext.STUDENTs.First(student => student.Name.Equals("Raj Patel"));
+                STUDENT yashPatel = linqToDataClasses1DataContext.STUDENTs.First(student => student.Name.Equals("Yash Patel"));
+                STUDENT parasPatel = linqToDataClasses1DataContext.STUDENTs.First(student => student.Name.Equals("Paras Patel"));
+                STUDENT palakPatel = linqToDataClasses1DataContext.STUDENTs.First(student => student.Name.Equals("Palak Patel"));
+                STUDENT raniPatel = linqToDataClasses1DataContext.STUDENTs.First(student => student.Name.Equals("Rani Patel"));
+
+                Lecture math = linqToDataClasses1DataContext.Lectures.First(lecture => lecture.Name.Equals("Math"));
+                Lecture phy = linqToDataClasses1DataContext.Lectures.First(lecture => lecture.Name.Equals("Phy"));
+                Lecture chem = linqToDataClasses1DataContext.Lectures.First(lecture => lecture.Name.Equals("Chem"));
+                Lecture bio = linqToDataClasses1DataContext.Lectures.First(lecture => lecture.Name.Equals("Bio"));
+
+                linqToDataClasses1DataContext.StudentLectures.InsertOnSubmit(new StudentLecture { LectureID = math.Id, StudentID = bhavikVashi.Id });
+                linqToDataClasses1DataContext.StudentLectures.InsertOnSubmit(new StudentLecture { LectureID = phy.Id, StudentID = rajPatel.Id });
+                linqToDataClasses1DataContext.StudentLectures.InsertOnSubmit(new StudentLecture { LectureID = chem.Id, StudentID = yashPatel.Id });
+                linqToDataClasses1DataContext.StudentLectures.InsertOnSubmit(new StudentLecture { LectureID = math.Id, StudentID = parasPatel.Id });
+                linqToDataClasses1DataContext.StudentLectures.InsertOnSubmit(new StudentLecture { LectureID = bio.Id, StudentID = palakPatel.Id });
+                linqToDataClasses1DataContext.StudentLectures.InsertOnSubmit(new StudentLecture { LectureID = chem.Id, StudentID = raniPatel.Id });
+
+
+                linqToDataClasses1DataContext.SubmitChanges();
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            MainDataGrid.ItemsSource = linqToDataClasses1DataContext.StudentLectures;
+
+
+        }
+
+        public void GetBhavikUniversity()
+        {
+            STUDENT bhavikVashi = linqToDataClasses1DataContext.STUDENTs.First(student => student.Name.Equals("Bhavik Vashi"));
+            University bhavikUniversity = bhavikVashi.University;
+
+            List<University> universityList = new List<University>();
+            universityList.Add(bhavikUniversity);
+
+
+            MainDataGrid.ItemsSource = universityList;
 
         }
     }
